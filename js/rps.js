96e900f1892deb1b28599f8d7e2478c9fe1cd8ba
Garrett Maitland <gmaitlan18@gmail.com>
2025-05-27 15:16:30 -0400

    feat: prompt player for choice
    The player is prompted for their choice by a dialog box until they have entered a valid choice.

--- js/rps.js
+++ js/rps.js
@@ -3,9 +3,34 @@ let computerChoice = getComputerChoice();
 if (computerChoice !== undefined) {
   console.log(computerChoice);
 }
+let playerChoice = getPlayerChoice();
+
+while (playerChoice === null || playerChoice === "") {
+  console.warn("Player choice undefined");
+  playerChoice = getPlayerChoice();
+}
+
+console.log(playerChoice);
 
 function getRandomInteger(number) {
   return Math.floor(Math.random() * number);
+}
+
+function getPlayerChoice() {
+  let playerChoice = prompt("Rock, paper, scissors! SHOOT!");
+
+  if (playerChoice === null) {
+    return "";
+  }
+
+  playerChoice = playerChoice.toLowerCase();
+  if ((playerChoice === "rock")
+   || (playerChoice === "paper")
+   || (playerChoice === "scissors")) {
+    return playerChoice;
+  }
+
+  return "";
 }
 
 function getComputerChoice() {
