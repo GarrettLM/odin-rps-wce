8dc3bf1703e4bf66a3954059a9695b7bbb3cb8f5
Garrett Maitland <gmaitlan18@gmail.com>
2025-05-27 15:04:18 -0400

    feat: get random choice from computer

--- js/rps.js
+++ js/rps.js
@@ -1,1 +1,23 @@
 console.log("Rock Paper Scissors:\nWorld Championship Edition")
+let computerChoice = getComputerChoice();
+if (computerChoice !== undefined) {
+  console.log(computerChoice);
+}
+
+function getRandomInteger(number) {
+  return Math.floor(Math.random() * number);
+}
+
+function getComputerChoice() {
+  let choiceInteger = getRandomInteger(3);
+  switch (choiceInteger) {
+    case 0:
+      return "rock";
+    case 1:
+      return "paper";
+    case 2:
+      return "scissors";
+    default:
+      console.error("Unrecognized case: " + choiceInteger);
+  }
+}
