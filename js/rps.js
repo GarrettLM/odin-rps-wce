f5e93cc021a1d7754757369409425f78e37397d3
Garrett Maitland <gmaitlan18@gmail.com>
2025-05-27 15:36:33 -0400

    feat: check for winner of round

--- js/rps.js
+++ js/rps.js
@@ -1,24 +1,50 @@
+const PLAYER_WINS = 1;
+const COMPUTER_WINS = 2;
+const DRAW = 0;
+
 console.log("Rock Paper Scissors:\nWorld Championship Edition")
-let computerChoice = getComputerChoice();
-if (computerChoice !== undefined) {
-  console.log(computerChoice);
-}
-let playerChoice = getPlayerChoice();
+playRound();
 
-while (playerChoice === null || playerChoice === "") {
-  console.warn("Player choice undefined");
-  playerChoice = getPlayerChoice();
+function checkForWin(playerOneChoice, playerTwoChoice) {
+  return (((playerOneChoice === "rock") && (playerTwoChoice === "scissors"))
+    || ((playerOneChoice === "paper") && (playerTwoChoice === "rock"))
+    || ((playerOneChoice === "scissors") && (playerTwoChoice === "paper")));
 }
 
-console.log(playerChoice);
+function playRound() {
+  let playerChoice = getPlayerChoice();
+
+  while (playerChoice === null || playerChoice === "") {
+    console.warn("Player choice undefined");
+    playerChoice = getPlayerChoice();
+  }
+
+  let computerChoice = getComputerChoice();
+  if (computerChoice === undefined) {
+    console.error(computerChoice);
+    return;
+  }
+
+  console.log("Player: " + playerChoice);
+  console.log("Computer:" + computerChoice);
+
+  if (checkForWin(playerChoice, computerChoice)) {
+    console.log("Player wins!");
+    return PLAYER_WINS;
+  } else if (checkForWin(computerChoice, playerChoice)) {
+    console.log("Player loses...");
+    return COMPUTER_WINS;
+  } else {
+    console.log("Draw...");
+    return DRAW;
+  }
+}
 
 function getRandomInteger(number) {
   return Math.floor(Math.random() * number);
 }
 
-function getPlayerChoice() {
-  let playerChoice = prompt("Rock, paper, scissors! SHOOT!");
-
+function validatePlayerChoice(playerChoice) {
   if (playerChoice === null) {
     return "";
   }
@@ -31,6 +57,16 @@ function getPlayerChoice() {
   }
 
   return "";
+}
+
+function getPlayerChoice() {
+  let playerChoice;
+  do {
+    playerChoice = prompt("Rock, paper, scissors! SHOOT!");
+    playerChoice = validatePlayerChoice(playerChoice);
+  } while (playerChoice === "");
+
+  return playerChoice;
 }
 
 function getComputerChoice() {
